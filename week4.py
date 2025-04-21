35a12aaa9f2bb0007738fc1b35f7f42f55fa5a11
Mayank12Sharma <sharmaji123mayank@gmail.com>
2025-04-21 21:22:44 +0530

    Add Skills Boost Arcade Trivia data display in week4 function

--- week4.py
+++ week4.py
@@ -4,6 +4,23 @@ import pandas as pd
 
 def week4():
 
+    data2 = [
+        [
+            "Skills Boost Arcade Trivia April 2025 Week 4",
+            "[Badge Link](https://www.cloudskillsboost.google/games/6105?utm_source=qwiklabs&utm_medium=lp&utm_campaign=arcade25-April-trivia)",
+            "1q-trivia-04553",
+        ],
+    ]
+
+    # Convert to DataFrame
+    df2 = pd.DataFrame(
+        data2, columns=["Arcade Trivia/Game", "Badge Link", "access Code"]
+    )
+
+    col1, col2, col3 = st.columns([2, 3, 2])
+    with col2:
+        st.markdown(df2.to_markdown(index=False), unsafe_allow_html=True)
+
     # Data for Google Cloud Badges
     data = [
         [
@@ -99,13 +116,10 @@ def week4():
     ]
 
     # Convert to DataFrame
-    df = pd.DataFrame(
-        data, columns=["S. No", "Badge Name", "Badge Link"]
-    )
+    df = pd.DataFrame(data, columns=["S. No", "Badge Name", "Badge Link"])
 
     # Display Data in Two Columns
 
-    
     col1, col2 = st.columns(2)
     with col1:
         st.markdown(df[0:9].to_markdown(index=False), unsafe_allow_html=True)
